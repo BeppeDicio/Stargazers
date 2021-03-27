3104387b9b864c34154a6b981541273de796ec85
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-03-27 11:30:02 +0100

    [master] UI changes on SearchView

--- Stargazers/Stargazers/Views/SearchView.swift
+++ Stargazers/Stargazers/Views/SearchView.swift
@@ -10,19 +10,32 @@ import UIKit
 class SearchView: UIViewController {
 
     var apiService = ApiService()
+    @IBOutlet weak var searchRepoButton: UIButton!
+    @IBOutlet weak var ownerTextField: UITextField!
+    @IBOutlet weak var repositoryTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        // searchRepoButton settings
+        searchRepoButton.layer.cornerRadius = 25
         
     }
     
     @IBAction func SearchRepoButtonAction(_ sender: Any) {
-        
-        apiService.getStargazersData { (result) in
-            print(result)
+        errorLabel.alpha = 0
+        if(ownerTextField.text == "" || repositoryTextField.text == "") {
+            errorLabel.alpha = 1
+            errorLabel.text = "Attention! Please check if the Owner and the Repositoy are correct!"
+            print("one of the TextFields are null")
+            return
         }
+        /*apiService.getStargazersData { (result) in
+            print(result)
+        }*/
         
-        self.performSegue(withIdentifier: "resultViweSegue", sender: self)
+        //self.performSegue(withIdentifier: "resultViweSegue", sender: self)
     }
 }
